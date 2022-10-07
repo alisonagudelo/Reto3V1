7f45f1fc685b313af4d7096b891109690c7021eb
Alison Agudelo <alison18015@gmail.com>
2022-10-06 19:05:19 -0500

    third commit

--- src/main/java/com/example/demo/Interface/ReservationInterface.java
+++ src/main/java/com/example/demo/Interface/ReservationInterface.java
@@ -5,6 +5,9 @@
 package com.example.demo.Interface;
 
 import com.example.demo.Modelo.Reservation;
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -13,4 +16,12 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface ReservationInterface extends CrudRepository <Reservation, Integer>{
     
+    public List<Reservation> findAllByStatus(String status);
+    
+    public List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date datoUno, Date datodos);
+    
+    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client) DESC")
+    public List<Object[]> countTotalReservationsByClient();
+
+
 }
